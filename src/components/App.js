4dda3393334e5a91a8f16731a436744347d546a9
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-10-22 18:53:12 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  render() {
+    let relatives = ["vinod", "yogesh", "mahesh", "vaishali"];
+    return (
+      <>
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={"relative" + (index + 1)}>{relative}</li>
+          ))}
+        </ol>
+      </>
+    );
+  }
 }
-
 
 export default App;
